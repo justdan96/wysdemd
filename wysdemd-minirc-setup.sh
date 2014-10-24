af4700600f32067b1147b8a891d24ac960ed5749
Dan <justdan96@gmail.com>
2014-10-24 22:47:54 +0100

    cleanup

--- wysdemd-minirc-setup.sh
+++ wysdemd-minirc-setup.sh
@@ -27,20 +27,26 @@ echo "==> Linking busybox to /sbin/{init,halt,poweroff,reboot}"
 
 wget -N http://www.busybox.net/downloads/binaries/latest/busybox-x86_64 -O busybox
 chmod 777 busybox
-cp -f busybox-x86_64 /usr/sbin/busybox
-chmod 777 /usr/sbin/busybox
+mkdir -p /opt/wysdemd/
+cp -f busybox /opt/wysdemd/init
+chmod 777 /opt/wysdemd/init
 
 # I get errors with soft linking as the init is called as /sysroot/
 # so we copy it instead
-for i in init halt poweroff reboot; do
-    # ln -sf /usr/sbin/busybox "$ROOT"/sbin/$i
-    cp -f /usr/sbin/busybox "$ROOT"/sbin/$i
-done
 
-echo "==> Appending 'init=sbin/init' to your kernel line in your bootloader"
+echo "==> Append 'init=/opt/wysdemd/init' to your kernel line in your bootloader"
 
-grep -q -F 'init=sbin/init ' /etc/default/grub || sed -i.bak 's#GRUB_CMDLINE_LINUX="#GRUB_CMDLINE_LINUX="init=sbin/init #g' /etc/default/grub
+# grep -q -F 'init=sbin/init ' /etc/default/grub || sed -i.bak 's#GRUB_CMDLINE_LINUX="#GRUB_CMDLINE_LINUX="init=sbin/init #g' /etc/default/grub
 cp -r /boot/grub2/grub.cfg /boot/grub2/grub.cfg.bak
-grub2-mkconfig -o /boot/grub2/grub.cfg
+# grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # Run "./setup.sh --force" to use the script
+
+echo "==> Installing dshimv into /opt/wysdemd/dshimv"
+
+wget -M https://raw.githubusercontent.com/embolalia/dshimv/master/dshimv -O dshimv
+chmod 777 dshimv
+cp -f dshimv /opt/wysdemd/dshimv
+chmod 777 /opt/wysdemd/dshimv
+
+
