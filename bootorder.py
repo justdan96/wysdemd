dd965c09970f5f8f4a60b7b641cec903801e681d
root <root@localhost.localdomain>
2014-10-24 22:42:00 +0100

    Created the boot order script

---
+++ bootorder.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+from funcs import (check_env_file, check_for_file, check_for_service,
+check_for_specifiers, clear_semicolon_comment, replace_specifiers, 
+create_config, tree, dicts, tree_add, tree_set, toposort2)
+
+import ConfigParser
+import sys, os
+from types import StringType
+import tempfile
+import re
+import pprint
+
+try:
+    from functools import reduce
+except:
+    pass
+
+# -------------------------------------------
+# 
+# subroutine section
+# 
+# -------------------------------------------
+
+def read_require_fields_from_target_file(fil):
+    config, prog, is_service = create_config(fil)
+    
+    if config == 1:
+        # print 'Error initialising target file: ' + fil
+        return [1,1]
+	
+    options = ['After', 'Requires']
+    
+    requires = set()
+    
+    for option in options:
+        if config.has_option("Unit", option):
+            after_services_str = config.get("Unit", option)[0]
+            
+            for unit in after_services_str.split(" "):
+                if unit != "rescue.target" and unit != "rescue.service" and unit != "emergency.target" and unit != "emergency.service":
+                   requires.add(unit)
+                   # required_str += unit + " "
+                
+    # print prog + ": "
+    return requires, is_service
+    
+def read_wants_from_target_file(fil):
+    config, prog, is_service = create_config(fil)
+    
+    if config == 1:
+        # print 'Error initialising target file: ' + fil
+        return 1
+    
+    wants_path = '/etc/systemd/system/' + prog + '.target.wants/'
+
+    options = ['Wants']
+    
+    wants = set()
+
+    for option in options:
+        if config.has_option("Unit", option):
+            after_services_str = config.get("Unit", option)[0]
+
+            for unit in after_services_str.split(" "):
+                # ignore any references to rescue or emergency
+                if unit != "rescue.target" and unit != "rescue.service" and unit != "emergency.target" and unit != "emergency.service":
+                    wants.add(unit)
+        else:
+            break
+    
+    if not is_service and os.path.isdir(wants_path):
+       wantsfiles = [ f for f in os.listdir(wants_path) if os.path.isfile(os.path.join(wants_path,f)) ]
+       # add list to set
+       wants |= set(wantsfiles)
+    
+    # special case for "default.target"
+    if not is_service and prog == "default":
+       # get the real name of the target, i.e. graphical
+       real_target = os.path.basename(os.readlink('/etc/systemd/system/default.target')).replace(".target", "")
+       
+       # create the wants path for the real target, create the list of files and append them to the wants set
+       real_wants_path = '/etc/systemd/system/' + real_target + '.target.wants/'
+       
+       if os.path.isdir(real_wants_path):
+           real_wantsfiles = [ f for f in os.listdir(real_wants_path) if os.path.isfile(os.path.join(real_wants_path,f)) ]
+           wants |= set(real_wantsfiles)
+    
+    return wants
+
+def process_services(target):
+    reqs, is_service = read_require_fields_from_target_file(target)
+    if reqs != 1 and reqs:
+            for reqser in reqs:
+                # service = 'display-manager'
+                # reqser = ['systemd-user-sessions.service', 'getty@tty1.service', 'plymouth-quit.service', 'livesys-late.service']
+                process_services(reqser)
+            if target not in boot_services:
+                boot_services[target] = reqs
+
+def recurse_required_services(requires, required_services):
+    # print 'requires: ',
+    # print requires
+    if requires != 1:
+        for req in requires:
+            if req.split('.')[1] == 'target':
+                nreqs, is_service = read_require_fields_from_target_file(req)
+                recurse_required_services(nreqs, required_services)
+            if req.split('.')[1] == 'service':
+                required_services.add(req)
+
+def process_targets(target, target_str):
+    requires, is_service = read_require_fields_from_target_file(target)
+    
+    # print 'THIS TARGET IS = ' + target
+    
+    # if there are actually some requires
+    if requires != 1:
+        # add this particular target 
+            
+        # print requires
+        for reqtar in requires:
+            # add the requirements to the tree
+            target_path = target_str + '>' + reqtar
+            tree_add(boot_targets, target_path.split('>'))
+            
+            # if the target isn't in our set of all targets
+            # parse the target as well
+            if reqtar not in all_boot_targets:
+                all_boot_targets.add(reqtar)
+                process_targets(reqtar, target_path)
+        
+        # find the wants and process those targets too
+        wants = read_wants_from_target_file(target)
+        if wants and wants != 1:
+            for wantar in wants:
+                # print 'wantar: ' + wantar
+                wantar_path = target_str + '>' + wantar
+                process_targets(wantar, wantar_path)
+            # tree_set(boot_targets, target_str.split('>'), wants)
+        
+        # if this current target is a service, add it to the list of boot services
+        # making sure we add all the service requirements as dependencies 
+        if is_service == True:
+            if target not in boot_services:
+                boot_reqservs = set()
+                recurse_required_services(requires, boot_reqservs)
+                boot_services[target] = boot_reqservs
+
+# -------------------------------------------
+# 
+# main section
+# 
+# -------------------------------------------
+
+global boot_targets, all_boot_targets, default_target, boot_services
+
+boot_targets = tree()
+
+all_boot_targets = set()
+
+default_target = 'default.target'
+
+all_boot_targets.add(default_target)
+
+boot_targets[default_target]
+boot_targets_str = default_target
+
+boot_services = {}
+
+process_targets(default_target, default_target)
+
+#pprint.pprint(dicts(boot_targets))
+
+print "BOOT TARGETS:"
+pprint.pprint(dicts(boot_targets))
+# print boot_targets
+
+print "ALL BOOT TARGETS:"
+print all_boot_targets
+
+print "BOOT SERVICES:"
+pprint.pprint(boot_services)
+
+print "SORTED BOOT SERVICES:"
+print (' '.join( toposort2(boot_services) ))
+
+#boot_targets = new Tree()
+#boot_targets.add(default_target)
+
+#all_boot_targets = new Set()
+#all_boot_targets.add(default_target)
+
+#boot_services = new Set()
+
+#process_targets(default_target)
+
+#topological_sort(boot_services)
+
+#def process_targets(target):
+   #requires = read_require_fields_from_target_file(target)
+   #for reqtar in requires:
+      ## add a sub-leaf "reqtar" to leaf "target" 
+      #boot_targets.add(target.reqtar)
+      #if reqtar not in all_boot_targets:
+         #all_boot_targets.add(reqtar)
+         #process_targets(target + '.' + reqtar)
+   
+   #wants = read_wants_from_target(target)
+   #for wantar in wants:
+      #process_services(wantar)
+
+#def process_services(service):
+   #reqs = read_require_fields_from_service_file(service)
+   #for reqser in reqs:
+      ## service = 'display-manager'
+      ## reqser = ['systemd-user-sessions.service', 'getty@tty1.service', 'plymouth-quit.service', 'livesys-late.service']
+      #process_services(reqser)
+   #if service not in boot_services:
+      #boot_services.add(service, reqs)
+   
+
+#boot_targets.print()
+
---
+++ funcs.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import sys, os, tempfile, re, collections
+from types import StringType
+
+class newdict(dict):
+    def __setitem__(self, key, value):
+        if key in self:
+            if type(value) is not StringType:
+                dict.__setitem__(self, key, self[key] + value)
+        else:
+            if type(value) is StringType:
+                temp_list = []
+                temp_list.append(value)
+                value = temp_list
+            dict.__setitem__(self, key, value)
+
+def tree(): return collections.defaultdict(tree)
+
+def dicts(t): return {k: dicts(t[k]) for k in t}
+
+def tree_add(t, path):
+  for node in path:
+    t = t[node]
+
+def tree_set(t, path, val):
+    i = 0
+    for node in path:
+        i = i + 1
+        if i == len(path):
+            t[node] = str(val)
+
+def toposort2(data):
+    for k, v in data.items():
+        v.discard(k) # Ignore self dependencies
+    extra_items_in_deps = reduce(set.union, data.values()) - set(data.keys())
+    data.update({item:set() for item in extra_items_in_deps})
+    while True:
+        ordered = set(item for item,dep in data.items() if not dep)
+        if not ordered:
+            break
+        yield ' '.join(sorted(ordered))
+        data = {item: (dep - ordered) for item,dep in data.items()
+                if item not in ordered}
+    assert not data, "A cyclic dependency exists amongst %r" % data
+            
+def create_config(fil):
+    config = ConfigParser.ConfigParser(None, newdict)
+
+    if fil[0:20] != '/etc/systemd/system/' and fil[0:20] != '/lib/systemd/system/':
+        fil = '/etc/systemd/system/' + fil
+    if not os.path.isfile(fil):
+        fil = fil.replace("/etc/", "/lib/")
+        if not os.path.isfile(fil):
+           # print "Cannot find file!"
+           return [1,1,1]
+           
+    prog = (fil.split('/')[-1]).split('.')[0]
+    
+    if check_for_file(fil) == 0:
+		is_service = check_for_specifiers(fil, config, prog)
+        
+    return config, prog, is_service
+
+
+def check_env_file(Environment_file):
+    print "if test -f %s; then\n\t. %s \nfi\n" % (Environment_file,
+                                                  Environment_file)
+
+def check_for_specifiers(fil, config, prog):
+    '''
+    Function: check_for_specifiers()
+    --------------------------------
+    It checks for the specifiers mentioned in the systemd.unit man page.
+    throws a warning to the user. Else replaces the specifiers with
+    Checks the file name and if the name contains "@" but no instance it
+    appropriate values.
+
+    @return: No return value.
+
+    '''
+
+    # Extract the whole file content in a single string
+    # template_file, instance_name, prefix_name
+    conf_fd = open(fil, 'r')
+    conf_list = conf_fd.readlines()
+    conf_str = ''.join(conf_list)
+
+    conf_str = clear_semicolon_comment(conf_str)
+
+    if prog.find('@') != -1:
+        template_file = 1
+
+        if len(prog.split('@')[1]) == 0:
+            print "[WARNING] No instance name specified: Generated script may not be correct"
+            return 1
+
+        else:
+            # This is the value of %i/I
+            instance_name = prog.split('@')[1]
+            print instance_name
+            # This is the value of %p/P
+            prefix_name = prog.split('@')[0]
+            print prefix_name
+    else:
+        template_file = 0
+
+    conf_new_str = replace_specifiers(conf_str, template_file)
+    conf_new_fd = tempfile.NamedTemporaryFile(delete=False)
+    tempname = conf_new_fd.name
+    conf_new_fd.write(conf_new_str)
+    conf_new_fd.flush()
+    os.fsync(conf_new_fd)
+    conf_new_fd.close()
+
+    conf_new_fd = open(tempname, 'r')
+
+    try:
+        config.readfp(conf_new_fd)
+
+    except Exception, err:
+        print "Error:%s" % (str(err))
+        return 1
+
+    return check_for_service(config)
+
+
+def replace_specifiers(exec_str, template_file):
+    '''
+    Function: replace_specifiers(exec_str)
+    --------------------------------------
+    Checks for the occurence of common specifiers in the string and replaces
+    them with appropriate values.
+
+    @param exec_str: string which needs to be checked for specifiers
+    @return: The modified string with specifiers replaced or the same one if
+    none found.
+    '''
+
+    if template_file == 1:
+        # Check for %i/I specifiers and replace them with instance_name
+        if (exec_str.find('%i') != -1) or (exec_str.find('%I') != -1):
+            exec_str = exec_str.replace('%i', instance_name)
+            exec_str = exec_str.replace('%I', instance_name)
+
+        # Check for %p/P specifiers and replace them with prefix_name
+        elif (exec_str.find('%p') != -1) or (exec_str.find('%P') != -1):
+            exec_str.replace('%p', prefix_name)
+            exec_str.replace('%P', prefix_name)
+
+        elif exec_str.find('%f') != -1:
+            exec_str.replace('%p', '/' + instance_name)
+
+    # Check for %u/U and replace them with full unit name.
+    # Should mean 'prog'(doubtful)
+    if (exec_str.find('%u') != -1) or (exec_str.find('%U') != -1):
+        exec_str.replace('%u', prog)
+        exec_str.replace('%U', prog)
+
+    return exec_str
+
+def clear_semicolon_comment(file_str):
+    return re.sub(' *; *', ';', file_str)
+
+def check_for_file(fil):
+    try:
+        conf_fd = open(fil, 'r')
+
+    except Exception, err:
+        print err
+        return err
+    
+    return 0
+
+def check_for_service(config):
+    # Here we check if the file is a service file.
+    if config.has_section("Service") is True:
+        return True
+    else:
+        return False
+    #    print "Error: The configuration file isn't a service file"
+    #    sys.exit(1)
+
